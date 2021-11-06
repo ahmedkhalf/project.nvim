71d0e23dcfc43cfd6bb2a97dc5a7de1ab47a6538
ahmedkhalf <ahmedkhalf567@gmail.com>
2021-11-06 12:17:40 -0400

    Add stable api entry point for get_recent_projects

--- lua/project_nvim/init.lua
+++ lua/project_nvim/init.lua
@@ -1,6 +1,8 @@
 local config = require("project_nvim.config")
+local history = require("project_nvim.utils.history")
 local M = {}
 
 M.setup = config.setup
+M.get_recent_projects = history.get_recent_projects
 
 return M
